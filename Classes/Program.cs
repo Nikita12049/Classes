d7e9830a6d4e517fac794b71e08673f707d371d6
Nik <oknik2009@gmail.com>
2024-10-19 18:01:12 +0300

    set the parameters for the weding machine

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            VendingMachine vendingMachine = new VendingMachine();
+
+            vendingMachine.name = "Кофемашина";
+            vendingMachine.cashBalance = 0;
+            vendingMachine.amountOfWater = 2; //литра
+            vendingMachine.amountOfCoffee = 250; //грамм
+            vendingMachine.amountOfMilk = 1; //литр
+            vendingMachine.amountOfSugar = 150; //грамм
+            vendingMachine.salesAmounts = 0;
         }
     }
 }
