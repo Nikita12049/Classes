6ae3acfaef22da8f69ae1a960cced079bd399e52
Nik <oknik2009@gmail.com>
2024-10-19 17:42:11 +0300

    created parameters for the vending machine

--- Classes/VendingMachine.cs
+++ Classes/VendingMachine.cs
@@ -1,6 +1,13 @@
 ﻿namespace Classes
 {
-    internal class VendingMachine
+    public class VendingMachine
     {
+        public string name;
+        public double cashBalance;
+        public double amountOfWater;
+        public double amountOfCoffee;
+        public double amountOfMilk;
+        public double amountOfSugar;
+        public double salesAmounts;
     }
 }
