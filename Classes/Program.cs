3c5b0b52e54b71f433bed5aabe548104946b64be
Nik <oknik2009@gmail.com>
2024-10-21 18:51:42 +0300

    beautifully designed code

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,18 +1,12 @@
-﻿namespace Classes
+﻿using System.Xml.Linq;
+
+namespace Classes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            VendingMachine vendingMachine = new VendingMachine();
-
-            vendingMachine.name = "Кофемашина";
-            vendingMachine.cashBalance = 0;
-            vendingMachine.amountOfWater = 2; //литра
-            vendingMachine.amountOfCoffee = 250; //грамм
-            vendingMachine.amountOfMilk = 1; //литр
-            vendingMachine.amountOfSugar = 150; //грамм
-            vendingMachine.salesAmounts = 0;
+            VendingMachine coffeeMacine = new VendingMachine("Кофемашина", 0, 2 , 250, 1, 150, 0);
         }
     }
 }
--- Classes/VendingMachine.cs
+++ Classes/VendingMachine.cs
@@ -2,12 +2,23 @@
 {
     public class VendingMachine
     {
-        public string name { get; private set; }
-        public double cashBalance { get; private set; }
-        public double amountOfWater { get; private set; }
-        public double amountOfCoffee { get; private set; }
-        public double amountOfMilk { get; private set; }
-        public double amountOfSugar { get; private set; }
-        public double salesAmounts { get; private set; }
+        public string Name { get; private set; }
+        public double CashBalance { get; private set; }
+        public double AmountOfWater { get; private set; }
+        public double AmountOfCoffee { get; private set; }
+        public double AmountOfMilk { get; private set; }
+        public double AmountOfSugar { get; private set; }
+        public double SalesAmounts { get; private set; }
+
+        public VendingMachine(string name, double cashBalance, double amountOfWater, double amountOfCoffee, double amountOfMilk, double amountOfSugar, double salesAmounts)
+        {
+            this.Name = name;
+            this.CashBalance = cashBalance;
+            this.AmountOfWater = amountOfWater;
+            this.AmountOfCoffee = amountOfCoffee;
+            this.AmountOfMilk = amountOfMilk;
+            this.AmountOfSugar = amountOfSugar;
+            this.SalesAmounts = salesAmounts;
+        }
     }
 }
